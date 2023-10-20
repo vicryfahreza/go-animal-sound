4ce86ae8e3df8a978a31441a9be4e7835192631b
vicryfahreza <vfahreza49@gmail.com>
2023-10-20 17:48:10 +0700

    Membuat module go_animal_sound

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vicryfahreza/go-animal-sound~
+
+go 1.21.0
---
+++ go_animal_sound.go
@@ -0,0 +1,17 @@
+package goanimalsound
+
+func DogSound() string {
+	return "Barkk"
+}
+
+func CatSound() string {
+	return "Meoww"
+}
+
+func DuckSound() string {
+	return "Kwekk Kwekk"
+}
+
+func DragonSound() string {
+	return "Grrrrr brawrrr"
+}
